f51ec9607f27ef61a3c9ceb76d926130e08cb68c
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-04-21 00:08:02 +0200

    Create states1.php

---
+++ Examples/states1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example states 1</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <h1>Associative arrays, long form</h1>
+
+    <p>
+        <?php
+        $capital['AL'] = "Alabama";
+        $capital['AK'] = "Alaska";
+        $capital['AZ'] = "Arizona";
+        $capital['AR'] = "Arkansas";
+        $capital['CA'] = "California";
+        $capital['CO'] = "Colorado";
+        $capital['CT'] = "Connecticut";
+        $capital['DE'] = "Delaware";
+        $capital['FL'] = "Florida";
+        $capital['GA'] = "Georgia";
+        $capital['HI'] = "Hawaii";
+        $capital['ID'] = "Idaho";
+        $capital['IL'] = "Illinois";
+        $capital['IN'] = "Indiana";
+        $capital['IA'] = "Iowa";
+        $capital['KS'] = "Kansas";
+        $capital['KY'] = "Kentucky";
+        $capital['LA'] = "Louisiana";
+        $capital['ME'] = "Maine";
+        $capital['MD'] = "Maryland";
+        $capital['MA'] = "Massachusetts";
+        $capital['MI'] = "Michigan";
+        $capital['MN'] = "Minnesota";
+        $capital['MS'] = "Mississippi";
+        $capital['MO'] = "Missouri";
+        $capital['MT'] = "Montana";
+        $capital['NE'] = "Nebraska";
+        $capital['NV'] = "Nevada";
+        $capital['NH'] = "New Hampshire";
+        $capital['NJ'] = "New Jersey";
+        $capital['NM'] = "New Mexico";
+        $capital['NY'] = "New York";
+        $capital['NC'] = "North Carolina";
+        $capital['ND'] = "North Dakota";
+        $capital['OH'] = "Ohio";
+        $capital['OK'] = "Oklahoma";
+        $capital['OR'] = "Oregon";
+        $capital['PA'] = "Pennsylvania";
+        $capital['RI'] = "Rhode Island";
+        $capital['SC'] = "South Carolina";
+        $capital['SD'] = "South Dakota";
+        $capital['TN'] = "Tennessee";
+        $capital['TX'] = "Texas";
+        $capital['UT'] = "Utah";
+        $capital['VT'] = "Vermont";
+        $capital['VA'] = "Virginia";
+        $capital['WA'] = "Washington";
+        $capital['WV'] = "West Virginia";
+        $capital['WI'] = "Wisconsin";
+        $capital['WY'] = "Wyoming";
+
+        echo $capital['CA'];
+        ?>
+    </p>
+</body>
+</html>
