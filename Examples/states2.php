916afb001e7cc1f8eb87ce56723232faf63a1823
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-04-21 00:08:57 +0200

    Create states2.php

---
+++ Examples/states2.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example states 2</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <h1>Associative arrays, short form</h1>
+
+    <p>
+        <?php
+        $capital = array(
+            "AL" => "Alabama",
+            "AK" => "Alaska",
+            "AZ" => "Arizona",
+            "AR" => "Arkansas",
+            "CA" => "California",
+            "CO" => "Colorado",
+            "CT" => "Connecticut",
+            "DE" => "Delaware",
+            "FL" => "Florida",
+            "GA" => "Georgia",
+            "HI" => "Hawaii",
+            "ID" => "Idaho",
+            "IL" => "Illinois",
+            "IN" => "Indiana",
+            "IA" => "Iowa",
+            "KS" => "Kansas",
+            "KY" => "Kentucky",
+            "LA" => "Louisiana",
+            "ME" => "Maine",
+            "MD" => "Maryland",
+            "MA" => "Massachusetts",
+            "MI" => "Michigan",
+            "MN" => "Minnesota",
+            "MS" => "Mississippi",
+            "MO" => "Missouri",
+            "MT" => "Montana",
+            "NE" => "Nebraska",
+            "NV" => "Nevada",
+            "NH" => "New Hampshire",
+            "NJ" => "New Jersey",
+            "NM" => "New Mexico",
+            "NY" => "New York",
+            "NC" => "North Carolina",
+            "ND" => "North Dakota",
+            "OH" => "Ohio",
+            "OK" => "Oklahoma",
+            "OR" => "Oregon",
+            "PA" => "Pennsylvania",
+            "RI" => "Rhode Island",
+            "SC" => "South Carolina",
+            "SD" => "South Dakota",
+            "TN" => "Tennessee",
+            "TX" => "Texas",
+            "UT" => "Utah",
+            "VT" => "Vermont",
+            "VA" => "Virginia",
+            "WA" => "Washington",
+            "WV" => "West Virginia",
+            "WI" => "Wisconsin",
+            "WY" => "Wyoming"
+        );
+
+        echo "The abbreviation CA stands for the federal state " . $capital['CA'] . ".";
+        ?>
+    </p>
+</body>
+</html>
