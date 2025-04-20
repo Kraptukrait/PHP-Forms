c1d758ef3e374a2eb185c5f476c35c0b96b3d05c
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-04-21 00:04:20 +0200

    Create hello.php

---
+++ Examples/hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <h1>The famous hello world script:</h1>
+
+    <p>
+        <?php
+        echo "<b>Hello</b> World!";
+        ?>
+    </p>
+
+</body>
+</html>
