5c42834558021fa20d92d304754fee4cde8e5ead
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-04-21 00:05:32 +0200

    Create variable1.php

---
+++ Examples/variable1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example Variable 1</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <h1>Output of variables</h1>
+
+    <p>
+        <?php
+        $name = "Deacon";
+        echo "Hello <b>$name</b>, Welcome!";
+        ?>
+    </p>
+
+</body>
+</html>
