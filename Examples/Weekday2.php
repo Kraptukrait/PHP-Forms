e08b21c628a41c5a469e24dc5aec79d27ca0d7ec
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-04-21 00:01:47 +0200

    Create Weekday2.php

---
+++ Examples/Weekday2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example Weekday 2</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <h1>Array in short form</h1>
+
+    <p>
+        <?php
+        $day = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+        
+        // Displaying Wednesday (index 3)
+        echo $day[3];
+        ?>
+    </p>
+
+</body>
+</html>
