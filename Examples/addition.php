e6e1eeb384affb069bd47bbebe89724abccd2d34
Kraptukrait <148796785+Kraptukrait@users.noreply.github.com>
2025-05-23 20:56:43 +0200

    Create addition.php

---
+++ Examples/addition.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Getting started with PHP: Lesson 3</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <h1>Simple addition with PHP</h1>
+
+    <p>
+        <?php
+        $number_a = 10;
+        $number_b = 35;
+        $result = $number_a + $number_b;
+        echo $result;
+        ?>
+    </p>
+
+</body>
+</html>
